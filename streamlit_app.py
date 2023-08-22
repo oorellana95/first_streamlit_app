012b291c301c864a874b94cf807fe22306e4a47e
Oscar <67627437+oorellana95@users.noreply.github.com>
2023-08-22 16:27:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,20 @@ fruits_choosen = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_choosen]
 streamlit.dataframe(fruits_to_show) # Display the table on the page.
 
+def get_fruityvie_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())     # normalize the json semi-structured data to structured data
+    return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
+    streamlit.error('Please select a fruit to get information.)
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())     # normalize the json semi-structured data to structured data
-    streamlit.dataframe(fruityvice_normalized)    # Display the table on the page.
+    fruityvice_data = get_fruityvie_data(fruit_choice)
+    streamlit.dataframe(fruityvice_data)
+
 except URLError as e:
   streamlit.error()
   
