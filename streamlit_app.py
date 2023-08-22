92a90c76f7c922e5010c1dce3bc51d1ae75bb909
Oscar <67627437+oorellana95@users.noreply.github.com>
2023-08-22 14:50:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+# normalize the json semi-structured data to structured data
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Display the table on the page.
+streamlit.dataframe(fruityvice_normalized)
